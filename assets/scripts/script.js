5f9ab535518ada0983ed01bd7399d6e0a501c1c4
John Doan <doanja@plu.edu>
2019-10-09 23:22:30 -0700

    working on render-detailed-recipe, created array to store response from detailed recipe endpoint

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,21 +1,66 @@
-const renderDetailedRecipe = recipeId => {
-  // render page using info from getRecipeInstructions(recipe.id, apiKey)
-  // TODO: create html elements for the detailed recipe (what information do we want displayed?)
-  // TODO: append the html elements
-  console.log('renderDetailedRecipe called on id ' + recipeId + ' [this function is WIP]');
+let recipeArr = []; // array used to store the response from the api
+
+/**
+ * function to add the class 'done' which changes the text to be crossed out
+ * when the checkbox is clicked
+ * @param {object} checkbox the html element input type=checkbox
+ * @param {object} div the element to be checked for the class 'done
+ */
+const addCheckboxClickListener = (checkbox, div) => {
+	const classDone = 'done';
+
+	// add click listeners
+	checkbox.click(() => {
+		if (div.hasClass(classDone)) {
+			div.removeClass(classDone);
+		} else {
+			div.addClass(classDone);
+		}
+	});
 };
 
+/**
+ * function to render the detailed recipe information
+ * @param {object} recipe the response object from the search endpoint from Spoonacular's API
+ */
+const renderDetailedRecipe = (recipe) => {
+	const div = $('<div>', { class: 'mt-3' });
+	const ul = $('<ul>', { class: 'list-group' });
+	const li = $('<li>', { class: 'list-group-item' }).text(recipe.title);
+	const checkbox = $('<input>', { type: 'checkbox' });
+
+	addCheckboxClickListener(checkbox, li);
+
+	$('#search-results').append(div);
+	div.append(ul);
+	ul.append(li);
+	li.append(checkbox);
+
+	// TODO: image title and images, append text after the checkbox
+};
+
+/**
+ * function that grabs the ID from the button clicked and passes that into
+ * renderDetailedRecipe to render the detailed recipe information
+ */
 function clickedRecipeDetails() {
-  // clear all search results
-  $('#search-results').empty();
+	// clear all search results
+	$('#search-results').empty();
 
-  // call getRecipeInstructions and pass in the ID
-  let recipeId = $(this).attr('id');
+	// call getRecipeInstructions and pass in the ID
+	const id = parseInt($(this).attr('id'));
 
-  //   console.log('ID: ', id);
+	// loop through recipeArr to find the response using the ID
+	for (let i = 0; i < recipeArr.length; i++) {
+		// compare the ID of the buttton to the ID of the objects inside recipeArr
+		if (recipeArr[i].id === id) {
+			// render the detailed recipe information
+			renderDetailedRecipe(recipeArr[i]);
 
-  // render the detailed recipe information
-  renderDetailedRecipe(recipeId);
+			// force exit loop upon finding a match
+			break;
+		}
+	}
 }
 
 /**
@@ -23,31 +68,35 @@ function clickedRecipeDetails() {
  * @param {object} recipe the response object from the search endpoint from Spoonacular's API
  */
 const renderSearchResults = (recipe, ingredients) => {
-  // create the html elements
-  const card = $('<div>', { class: 'card mt-3' });
-  const cardHeader = $('<div>', {
-    class: 'card-header bg-dark text-light'
-  }).text(recipe.title);
-  const cardBody = $('<div>', { class: 'card-body' });
-  const img = $('<img>', {
-    src: recipe.image,
-    class: 'rounded float-left mr-3'
-  });
-  const cardTitle = $('<h5>', { class: 'card-title' }).text(
-    recipe.readyInMinutes + ' minute(s) - Serving Size: ' + recipe.servings
-  );
+	// create the html elements
+	const card = $('<div>', { class: 'card mt-3' });
+	const cardHeader = $('<div>', {
+		class: 'card-header bg-dark text-light'
+	}).text(recipe.title);
+	const cardBody = $('<div>', { class: 'card-body' });
+	const img = $('<img>', {
+		src: recipe.image,
+		class: 'rounded float-left mr-3'
+	});
+	const cardTitle = $('<h5>', { class: 'card-title' }).text(
+		recipe.readyInMinutes + ' minute(s) - Serving Size: ' + recipe.servings
+	);
 
-  const cardText = $('<p>', { class: 'card-text' }).text('Ingrediants ' + ingredients);
-  const cardRate = $('<h5.', { class: 'health-score' }).text(recipe.healthScore);
-  const btn = $('<button>', {
-    class: 'btn btn-primary recipe-details-button',
-    id: recipe.id
-  }).text('View Recipe Details');
+	const cardText = $('<p>', { class: 'card-text' }).text(
+		'Ingrediants ' + ingredients
+	);
+	const cardRate = $('<h5.', { class: 'health-score' }).text(
+		recipe.healthScore
+	); // append this somewhere ?
+	const btn = $('<button>', {
+		class: 'btn btn-primary recipe-details-button',
+		id: recipe.id
+	}).text('View Recipe Details');
 
-  // append elements to the html
-  $('#search-results').append(card);
-  card.append(cardHeader, cardBody);
-  cardBody.append(img, cardTitle, cardText, btn);
+	// append elements to the html
+	$('#search-results').append(card);
+	card.append(cardHeader, cardBody);
+	cardBody.append(img, cardTitle, cardText, cardRate, btn);
 };
 
 /**
@@ -55,24 +104,27 @@ const renderSearchResults = (recipe, ingredients) => {
  * the search results
  */
 const getInput = () => {
-  // check if .form-control is empty and alert user
-  if (!$('.form-control').val()) {
-    alert('You must enter something to search...');
-  } else {
-    // clear any previous search results
-    $('#search-results').empty();
+	// empty out array
+	recipeArr = [];
 
-    // store search in input
-    const input = $('.form-control')
-      .val()
-      .trim();
+	// check if .form-control is empty and alert user
+	if (!$('.form-control').val()) {
+		alert('You must enter something to search...');
+	} else {
+		// clear any previous search results
+		$('#search-results').empty();
 
-    // clear text from .form-control
-    $('.form-control').val('');
+		// store search in input
+		const input = $('.form-control')
+			.val()
+			.trim();
 
-    // call getRecipe() and pass in input as a searchTerm for the first parameter
-    getRecipe(input, 2, SPOONACULAR_API_KEY);
-  }
+		// clear text from .form-control
+		$('.form-control').val('');
+
+		// call getRecipe() and pass in input as a searchTerm for the first parameter
+		getRecipe(input);
+	}
 };
 
 /**
@@ -83,33 +135,39 @@ const getInput = () => {
  * @param {string} id the id of the recipe
  * @param {string} apiKey the API key used to access spoonacular api
  */
-const getRecipeInstructions = (id, apiKey) => {
-  // the url past to the request header
-  console.log(id);
-  const url = 'https://api.spoonacular.com/recipes/' + id + '/information?includeNutrition=false' + '&apiKey=' + apiKey;
+const getRecipeInstructions = (id, apiKey = SPOONACULAR_API_KEY) => {
+	// the url past to the request header
+	const url =
+		'https://api.spoonacular.com/recipes/' +
+		id +
+		'/information?includeNutrition=false' +
+		'&apiKey=' +
+		apiKey;
 
-  // send a GET request to the detailed recipe endpoint
-  // (https://spoonacular.com/food-api/docs#Get-Recipe-Information)
-  $.ajax({
-    url,
-    method: 'GET'
-  })
-    .then(res => {
-      // string for ingredients
-      let ingredientsStr = '';
+	// send a GET request to the detailed recipe endpoint
+	// (https://spoonacular.com/food-api/docs#Get-Recipe-Information)
+	$.ajax({
+		url,
+		method: 'GET'
+	})
+		.then((res) => {
+			// string for ingredients
+			let ingredientsStr = '';
 
-      // loop through response and grab ingredients and append it to ingredientsStr
-      for (let i = 0; i < res.extendedIngredients.length; i++) {
-        ingredientsStr += res.extendedIngredients[i].name + ', ';
-      }
+			// loop through response and grab ingredients and append it to ingredientsStr
+			for (let i = 0; i < res.extendedIngredients.length; i++) {
+				ingredientsStr += res.extendedIngredients[i].name + ', ';
+			}
 
-      // remove any trailing commas
-      ingredientsStr = ingredientsStr.replace(/,\s*$/, '');
+			// remove any trailing commas
+			ingredientsStr = ingredientsStr.replace(/,\s*$/, '');
 
-      // render search results
-      renderSearchResults(res, ingredientsStr);
-    })
-    .catch(err => console.log('Error occured', err));
+			recipeArr.push(res);
+
+			// render search results
+			renderSearchResults(res, ingredientsStr);
+		})
+		.catch((err) => console.log('Error occured', err));
 };
 
 /**
@@ -118,32 +176,37 @@ const getRecipeInstructions = (id, apiKey) => {
  * @param {integer} limit the number of results returned from the request
  * @param {string} apiKey the API key used to access spoonacular api
  */
-const getRecipe = (searchTerm, limit = 10, apiKey) => {
-  // the url past to the request header
-  const url =
-    'https://api.spoonacular.com/recipes/search?query=' + searchTerm + '&number=' + limit + '&apiKey=' + apiKey;
+const getRecipe = (searchTerm, limit = 5, apiKey = SPOONACULAR_API_KEY) => {
+	// the url past to the request header
+	const url =
+		'https://api.spoonacular.com/recipes/search?query=' +
+		searchTerm +
+		'&number=' +
+		limit +
+		'&apiKey=' +
+		apiKey;
 
-  // send a GET request to the search endpoint
-  // (https://spoonacular.com/food-api/docs#Search-Recipes)
-  $.ajax({
-    url,
-    method: 'GET'
-  })
-    .then(res => {
-      // loop through the responses
-      for (let i = 0; i < res.results.length; i++) {
-        // pass the ID from the resonse to getRecipeInstructions()
-        getRecipeInstructions(res.results[i].id, apiKey);
-      }
-    })
-    .catch(err => console.log('Error occured', err));
+	// send a GET request to the search endpoint
+	// (https://spoonacular.com/food-api/docs#Search-Recipes)
+	$.ajax({
+		url,
+		method: 'GET'
+	})
+		.then((res) => {
+			// loop through the responses
+			for (let i = 0; i < res.results.length; i++) {
+				// pass the ID from the resonse to getRecipeInstructions()
+				getRecipeInstructions(res.results[i].id);
+			}
+		})
+		.catch((err) => console.log('Error occured', err));
 };
 
 window.onload = () => {
-  // listen for click
-  $('#search-button').click(getInput);
-  $(document).on('click', '.recipe-details-button', clickedRecipeDetails);
+	// listen for click
+	$('#search-button').click(getInput);
+	$(document).on('click', '.recipe-details-button', clickedRecipeDetails);
 
-  // getRecipe('cheese', 1, SPOONACULAR_API_KEY);
-  // getRecipeInstructions(21543, SPOONACULAR_API_KEY);
+	// getRecipe('cheese', 1, SPOONACULAR_API_KEY);
+	// getRecipeInstructions(21543, SPOONACULAR_API_KEY);
 };
